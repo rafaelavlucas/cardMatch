770ef4613bfbfd34500c4c5608842cea85abb2f7
Joao bairrada <bairrada1997@gmail.com>
2023-05-15 00:18:53 +0200

    fix issue while dragging card

--- app/components/CardGrid.vue
+++ app/components/CardGrid.vue
@@ -80,15 +80,14 @@ const loadCards = () => {
 
 function onEnd(evt: any) {
   const draggedElement = evt.item.__draggable_context.element;
-  console.log(evt.originalEvent.toElement.__draggable_context);
-  if (!evt.originalEvent.toElement.__draggable_context) return;
-  const toElement = evt.originalEvent.toElement.__draggable_context.element;
+
+  const toElement = evt.originalEvent.target.innerText;
   const findMatchFromList1 = list1.value.list.find(
     (card) => card.name === draggedElement.name
   );
 
   const findMatchFromList2 = list2.value.list.find(
-    (card) => card.name === toElement.name
+    (card) => card.name === toElement
   );
 
   if (
